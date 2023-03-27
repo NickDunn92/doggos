2e4588a463de239029113b8f243acad369963973
Nick Dunn <nickdunn2412@gmail.com>
2023-03-27 22:45:35 +0100

    trying to add event listeners to check if the user has entered a vale into the first name input

--- registerform.js
+++ registerform.js
@@ -1,23 +1,33 @@
+const form = document.querySelector("form");
 const inputs =  document.getElementsByTagName("input");
+const firstName = document.getElementById("firstname");
+const firstNameError = document.querySelector("#firstname + span.error");
 
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("click", function() {
     console.log("Click!");
-  })
+  });
 }
 
-// var inputClick = inputs[0].addEventListener("click", function onClick() {
-//   console.log("Clicked!");
-//   document.getElementsByClassName("sname");
-// })
+firstName.addEventListener("input", (event) => {
+  if (firstName.validity.valid) {
+    firstNameError.textContent("");
+    firstNameError.className("error");
+  } else {
+    showError();
+  }
+});
 
-// const inputList = document.querySelectorAll("input");
+form.addEventListener("submit", (event) => {
+  if (!firstName.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
 
-// // const inputArray = [...inputList];
-
-// // inputArray.forEach(input => {
-// //   console.log("Clicked!"); 
-// // })
-
-// inputs.addEventListener("click", (event) => {});
-// onclick = (event) => {};
+function showError() {
+  if (firstName.validity.valueMissing) {
+    firstNameError.textContent = "Please enter your first name"; 
+  } else (firstName.validity.typeMismatch) 
+    firstName.textContent = "Please enter a valid name";
+}
