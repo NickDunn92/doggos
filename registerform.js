5f01e59932644ccb06f1a09c65a22ad92c5f4cdc
Nick Dunn <nickdunn2412@gmail.com>
2023-03-28 13:42:42 +0100

    changed the to listen to user input when being entered

--- registerform.js
+++ registerform.js
@@ -7,27 +7,8 @@ for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("click", function() {
     console.log("Click!");
   });
-}
-
-firstName.addEventListener("input", (event) => {
-  if (firstName.validity.valid) {
-    firstNameError.textContent("");
-    firstNameError.className("error");
-  } else {
-    showError();
-  }
+  inputs[i].addEventListener('input', function (evt) { 
+  something(this.value);
 });
+};
 
-form.addEventListener("submit", (event) => {
-  if (!firstName.validity.valid) {
-    showError();
-    event.preventDefault();
-  }
-});
-
-function showError() {
-  if (firstName.validity.valueMissing) {
-    firstNameError.textContent = "Please enter your first name"; 
-  } else (firstName.validity.typeMismatch) 
-    firstName.textContent = "Please enter a valid name";
-}
