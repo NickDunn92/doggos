f5efbf74134e01833d0504ee50f015fc39c70e84
Nick Dunn <nickdunn2412@gmail.com>
2023-04-01 23:21:11 +0100

    changed code to display the value of the input with the id firstname to the console

--- registerform.js
+++ registerform.js
@@ -1,15 +1,13 @@
 const form = document.querySelector("form");
 const inputs =  document.getElementsByTagName("input");
-const firstName = document.getElementById("firstname");
-const firstNameError = document.querySelector("#firstname + span.error");
 
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("click", function() {
     console.log("Click!");
   });
   inputs[i].addEventListener('input', function() { 
-    document.getElementById("inputText").value;
-    console.log("Hi!");
+    const firstName = document.getElementById("firstname").value;
+    console.log(firstName);
   });
 };
 
